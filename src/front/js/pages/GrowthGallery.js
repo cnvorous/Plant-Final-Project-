212c26007193c1a3059719dca5399c52afbad1b6
cnvorous <cnvorous@gmail.com>
2022-03-06 22:27:01 +0000

    added pages to template set up

---
+++ src/front/js/pages/GrowthGallery.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+
+export const GrowthGallery = props => {
+	const { store, actions } = useContext(Context);
+	const params = useParams();
+
+	return (
+		<div>
+	
+			<Link to="/">
+				<span className="btn btn-primary btn-lg" href="#" role="button">
+					Back home
+				</span>
+			</Link>
+		</div>
+	);
+};
+
+GrowthGallery.propTypes = {
+	match: PropTypes.object
+};
