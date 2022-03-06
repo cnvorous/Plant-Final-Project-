e81aad6d24d94fb156059855f7a9903a4d1eb521
cnvorous <cnvorous@gmail.com>
2022-03-06 22:53:10 +0000

    added layout Routes

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -9,6 +9,14 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { CreateAccount } from "./pages/CreateAccount";
+import { Favorites } from "./pages/Favorites";
+import { SearchPlantData } from "./pages/SearchPlantData";
+import { SinglePlantView } from "./pages/SinglePlantView";
+import { GrowthGallery } from "./pages/GrowthGallery";
+import { Calendar } from "./pages/Calendar";
+import { GreenThumbGallery } from "./pages/GreenThumbGallery";
+import { Profile } from "./pages/Profile";
 
 //create your first component
 const Layout = () => {
@@ -22,15 +30,19 @@ const Layout = () => {
 				<ScrollToTop>
 					<Navbar />
 					<Switch>
-						<Route exact path="/">
-							<Home />
-						</Route>
-						<Route exact path="/demo">
-							<Demo />
-						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
-						</Route>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/demo" component={Demo} />
+						<Route exact path="/single/:theid" component={Single} />
+
+						<Route exact path="/single/:theid" component={CreateAccount} />
+						<Route exact path="/single/:theid" component={Profile} />
+						<Route exact path="/single/:theid" component={Favorites} />
+						<Route exact path="/single/:theid" component={SearchPlantData} />
+						<Route exact path="/single/:theid" component={SinglePlantView} />
+						<Route exact path="/single/:theid" component={GrowthGallery} />
+						<Route exact path="/single/:theid" component={Calendar} />
+						<Route exact path="/single/:theid" component={GreenThumbGallery} />
+
 						<Route>
 							<h1>Not found!</h1>
 						</Route>
