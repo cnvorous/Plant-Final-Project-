fb583ef3e33b6fbdd0c68d7d4f7fa209372650e6
Humajiva <humajiva123@gmail.com>
2022-03-14 00:17:56 +0000

    worked on account profile

--- src/front/js/pages/CreateAccount.js
+++ src/front/js/pages/CreateAccount.js
@@ -10,10 +10,22 @@ export const CreateAccount = props => {
 
 	return (
 		<div>
-			< div className="text-center">
+			<div className="text-center">
 				Create you account log in and user profile
+				<div className="profile " name="myForm" action="/action_page.php" onsubmit="return validateForm()" method="post">
+					<h1>Username/Email: <input type="text" name="fname" /></h1>
+					<h2>Password: <input type="text" name="fname" /></h2>
+					<h3>Firstname: <input type="text" name="fname" /></h3>
+					<h4>Lastname: <input type="text" name="fname" /></h4>
+					<h5>phone: <input type="text" name="fname" /></h5>
+					<h6>State/Zip code: <input type="text" name="fname" /></h6>
+					<span><input type="submit" value="Submit" /></span>
+				</div>
 
 			</div>
+
+
+
 
 			<Link to="/">
 				<span className="btn btn-primary btn-sm" href="#" role="button">
--- src/front/js/pages/Dashboard.js
+++ src/front/js/pages/Dashboard.js
@@ -12,56 +12,44 @@ export const Dashboard = (props) => {
     <div>
       <p className="text-center">Dashboard Profile Page</p>
 
-<<<<<<< HEAD
 
-			<Link to="/">
-				<span className="btn btn-primary btn-sm" href="#" role="button">
-					Back home
-				</span>
-			</Link>
-		</div>
-	);
-=======
+
       <Link to="/">
         <span className="btn btn-primary btn-sm" href="#" role="button">
           Back home
         </span>
       </Link>
-      <div class="list-group">
-        <a
-          href="#"
-          class="list-group-item list-group-item-action active"
-          aria-current="true"
-        >
-          Lets Find Some Plants!!!
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          Favorites List
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          Plant Growth Gallery
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          A fourth link item
-        </a>
-        <a class="list-group-item list-group-item-action disabled">
-          A disabled link item
-        </a>
-      </div>
     </div>
   );
->>>>>>> 06c0d63f962237ee9cbffe51c6abae87eed56795
+
+
+  <div class="list-group">
+    <a
+      href="#"
+      class="list-group-item list-group-item-action active"
+      aria-current="true"
+    >
+      Lets Find Some Plants!!!
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">
+      Favorites List
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">
+      Plant Growth Gallery
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">
+      A fourth link item
+    </a>
+    <a class="list-group-item list-group-item-action disabled">
+      A disabled link item
+    </a>
+  </div>
+
+
+
 };
 
-<div class="list-group">
-	<a href="#" class="list-group-item list-group-item-action active" aria-current="true">
-		The current link item
-	</a>
-	<a href="#" class="list-group-item list-group-item-action">A second link item</a>
-	<a href="#" class="list-group-item list-group-item-action">A third link item</a>
-	<a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
-	<a class="list-group-item list-group-item-action disabled">A disabled link item</a>
-</div>
+
 
 Dashboard.propTypes = {
   match: PropTypes.object,
