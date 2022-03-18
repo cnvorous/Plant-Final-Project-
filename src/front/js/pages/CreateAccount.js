60a76ca25e0802bfd3eb22ac2138c012c39d355e
Humajiva <humajiva123@gmail.com>
2022-03-18 20:23:43 +0000

    worked on login page and showing data in console

--- src/front/js/pages/CreateAccount.js
+++ src/front/js/pages/CreateAccount.js
@@ -8,6 +8,20 @@ import { UploadImages } from "../component/UploadImages";
 export const CreateAccount = props => {
 	const { store, actions } = useContext(Context);
 	const params = useParams();
+	const profileArray = store.profile;
+	//*const [newProfile, setNewProfile] = useState({});//*
+	const [emailValue, setEmailValue] = useState("");
+	const [passwordValue, setPasswordValue] = useState("");
+	const [firstNameValue, setFirstNameValue] = useState("");
+	const [lastNameValue, setLastNameValue] = useState("");
+	const [phoneValue, setPhoneValue] = useState("");
+	const [stateZipCodeValue, setStateZipCodeValue] = useState("");
+
+	const handleSubmit = () => {
+		profileArray.push({ email: emailValue, password: passwordValue, firstName: firstNameValue, lastName: lastNameValue, phone: phoneValue, stateZipCode: stateZipCodeValue });
+
+	}
+
 
 	return (
 		<div>
@@ -22,27 +36,34 @@ export const CreateAccount = props => {
 
 				<div className="p-5 d-flex justify-content-spacebetween row">
 					<div className="col-6 mb-3">
-						<input className="w-75" type="text" placeholder="Username/Email" name="fname" />
+						<input className="w-75" type="text" onChange={(e) => setEmailValue(e.target.value)} placeholder="Username/Email" name="fname" />
 					</div>
 					<div className="col-6 mb-3">
-						<input className="w-75" type="text" placeholder="Password" name="fname" />
+						<input className="w-75" type="text" onChange={(e) => setPasswordValue(e.target.value)} placeholder="Password" name="fname" />
 					</div>
 					<div className="col-6 mb-3">
-						<input className="w-75" type="text" placeholder="First Name" name="fname" />
+						<input className="w-75" type="text" onChange={(e) => setFirstNameValue(e.target.value)} placeholder="First Name" name="fname" />
 					</div>
 					<div className="col-6 mb-3">
-						<input className="w-75" type="text" placeholder="Last Name" name="fname" />
+						<input className="w-75" type="text" onChange={(e) => setLastNameValue(e.target.value)} placeholder="Last Name" name="fname" />
 					</div>
 					<div className="col-6 mb-3">
-						<input className="w-75" type="text" placeholder="Phone" name="fname" />
+						<input className="w-75" type="text" onChange={(e) => setPhoneValue(e.target.value)} placeholder="Phone" name="fname" />
 					</div>
 					<div className="col-6 mb-3">
-						<input className="w-75" type="text" placeholder="State/Zip code" name="fname" />
+						<input className="w-75" type="text" onChange={(e) => setStateZipCodeValue(e.target.value)} placeholder="State/Zip code" name="fname" />
 					</div>
 					<div><input type="checkbox" />
 						<label>Check to receive SMS notifications</label>
 					</div>
-					<input className=" submitButton btn btn-primary" type="submit" value="Submit" />
+					<input className=" submitButton btn btn-primary" onClick={() => {
+						handleSubmit();
+
+					}} type="submit" value="Submit" />
+					<input className=" submitButton btn btn-primary" onClick={() => {
+						console.log("this is the array on the flux:", store.profile)
+
+					}} type="submit" value="show on console" />
 					<div>
 						<Link to="/forgotPassword">
 							<a className="nav-link">Forgot my password??</a>
