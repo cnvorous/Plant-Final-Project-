edf310d54a8578e7ca546c505a99206b25f932f3
cnvorous <cnvorous@gmail.com>
2022-04-01 18:30:37 +0000

    changed table input characters, issue with Text

--- src/api/models.py
+++ src/api/models.py
@@ -3,17 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    #is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-	first_name=db.Column(db.String(70), nullable=False)
-	last_name=db.Column(db.String(70), nullable=False)
-	phone=db.Column(db.String(30), nullable=False)
-	sms_checkbox=db.Column(db.Boolean(), unique=False, nullable=False)
-	zipcode=db.Column(db.String(15), nullable=False)
-	profile_image=db.Column(db.String(250), nullable=True)
-	
+    id=db.Column(db.Integer, primary_key=True)
+    email=db.Column(db.String(120), unique=True, nullable=False)
+    password=db.Column(db.String(80), unique=False, nullable=False)
+    first_name=db.Column(db.String(70), nullable=False)
+    last_name=db.Column(db.String(70), nullable=False)
+    phone=db.Column(db.String(30), nullable=False)
+    sms_checkbox=db.Column(db.Boolean(), unique=False, nullable=False)
+    zipcode=db.Column(db.String(15), nullable=False)
+    profile_image=db.Column(db.String(250), nullable=True)
+    
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -24,11 +23,11 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
             "first_name": self.first_name,
-	        "last_name": self.last_name,
-        	"phone": self.phone,
-	        "sms_checkbox": self.sms_checkbox,
-	        "zipcode": self.zipcode,
-	        "profile_image": self.profile_image,
+            "last_name": self.last_name,
+            "phone": self.phone,
+            "sms_checkbox": self.sms_checkbox,
+            "zipcode": self.zipcode,
+            "profile_image": self.profile_image,
         }
 
         ################################
@@ -36,25 +35,25 @@ class User(db.Model):
 
 
 class Plant(db.Model):
-	id= db.Column(db.Integer, primary_key=True)
-	scientific_name= db.Column(db.String(250), nullable=False)
+    id=db.Column(db.Integer, primary_key=True)
+    scientific_name=db.Column(db.String(250), nullable=False)
     common_name=db.Column(db.String(250), nullable=False)
-    size=db.Column(db.String(250), nullable=False)
-    height=db.Column(db.String(250), nullable=False) 
+    size=db.Column(db.String(50), nullable=False)
+    height=db.Column(db.String(50), nullable=False) 
     blooms=db.Column(db.String(250), nullable=False)
     seasonal=db.Column(db.String(250), nullable=False) 
     growth_period=db.Column(db.String(250), nullable=False)
     light_exposure=db.Column(db.String(250), nullable=False)
-    most_suitable_location=db.Column(db.String(250), nullable=False)
-    temperature=db.Column(db.String(250), nullable=False)
-    plant_image=db.Column(db.String(250), nullable=False)
-    water_req=db.Column(db.String(250), nullable=False) 
-    water_tips=db.Column(db.String(250), nullable=False)
-    fertilizer_req=db.Column(db.String(250), nullable=False)
-    fertilizer_tips=db.Column(db.String(250), nullable=False)
-    poisonous=db.Column(db.String(250), nullable=False)
-    plant_details=db.Column(db.String(250), nullable=False)
-    plant_care_tips=db.Column(db.String(250), nullable=False)
+    most_suitable_location=db.Column(db.Text(500), nullable=False)
+    temperature=db.Column(db.String(50), nullable=False)
+    plant_image=db.Column(db.Text(500), nullable=False)
+    water_req=db.Column(db.String(50), nullable=False) 
+    water_tips=db.Column(db.Text(700), nullable=False)
+    fertilizer_req=db.Column(db.String(100), nullable=False)
+    fertilizer_tips=db.Column(db.Text(700), nullable=False)
+    poisonous=db.Column(db.Text(700), nullable=False)
+    plant_details=db.Column(db.Text(700), nullable=False)
+    plant_care_tips=db.Column(db.Text(700), nullable=False)
 
 
 
@@ -63,9 +62,9 @@ class Plant(db.Model):
 
     def serialize(self):
         return {
-           	"id": self.id,
-	        "scientific_name": self.scientific_name
-            "common_name": self.common_name
+               "id": self.id,
+            "scientific_name": self.scientific_name,
+            "common_name": self.common_name,
             "size": self.size,
             "height": self.height,
             "blooms": self.blooms,
@@ -87,11 +86,11 @@ class Plant(db.Model):
 
 
 class Favorites(db.Model):
-	id= db.Column(db.Integer, primary_key=True)
-    plant_id= db.Column(db.Integer, db.ForeignKey('plant.id'))
-    user_id=db.Column(db.Integer, ForeignKey('user.id'))
-    list_name=db.Column(db.String(250), nullable=True) #would come from user 
-	#favorite_plant=Column(db.String(250), nullable=True) # would come from user 
+    id=db.Column(db.Integer, primary_key=True)
+    plant_id=db.Column(db.Integer, db.ForeignKey('plant.id'))
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+    list_name=db.Column(db.String(40), nullable=True) #would come from user 
+    #favorite_plant=Column(db.String(250), nullable=True) # would come from user 
     #plant= relationship(Plant)
 
     def __repr__(self):
@@ -99,9 +98,8 @@ class Favorites(db.Model):
 
     def serialize(self):
         return {
-           	"id": self.id,
-	        "list_name": self.list_name,
+               "id": self.id,
+            "list_name": self.list_name,
             #"favorites": 
-    
         }
 
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Plant, Favorites
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,47 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/plants', methods=['GET'])
+def get_plants():
+    plant_query = Plant.query.all()
+    all_plants = list(map(lambda x: x.serialize(), plant_query))
+
+    #all_plants= [plant.serialize() for plant in plant_query]  this means same thing as line 23
+
+    return jsonify(all_plants), 200
+
+
+@api.route('/plants', methods=['POST'])
+def post_plants():
+    body=request.get_json()
+    print(body)
+    for item in body:
+        each_plant= Plant(
+            blooms= item['blooms'],
+            common_name= item['common_name'],
+            fertilizer_req= item['fertilizer_req'],
+            fertilizer_tips= item['fertilizer_tips'],
+            growth_period= item['growth_period'],
+            height= item['height'],
+            light_exposure= item['light_exposure'],
+            most_suitable_location= item['most_suitable_location'],
+            plant_care_tips= item['plant_care_tips'], 
+            plant_details= item['plant_details'],
+            plant_image= item['plant_image'], 
+            poisonous=item['poisonous'], 
+            scientific_name=item['scientific_name'], 
+            seasonal= item['seasonal'],
+            size= item['size'], 
+            temperature= item['temperature'],
+            water_req= item['water_req'], 
+            water_tips= item['water_tips'],
+            )
+
+        db.session.add(each_plant)
+        db.session.commit()
+
+    plant_query = Plant.query.all()
+    all_plants = list(map(lambda x: x.serialize(), plant_query))
+    return jsonify(all_plants), 200 
+
